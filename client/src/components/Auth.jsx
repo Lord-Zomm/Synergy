4ef2b193bd45bd298dc514a774735a7789db7423
Lord-Zomm <rafay.cheema@gmail.com>
2024-10-01 14:51:30 -0400

    d

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -28,7 +28,7 @@ const Auth = () => {
         const { username, password, phoneNumber } = form;
 
         //const URL = 'http://localhost:5000/auth';
-        const URL = 'http://synergy-45ua.onrender.com';
+        const URL = 'http://synergy-45ua.onrender.com/auth';
 
         const { data: { token, userId, hashedPassword, fullName } } = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`, {
             username, password, fullName: form.fullName, phoneNumber,
