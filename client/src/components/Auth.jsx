e03bddf66f9356d2a97febe1dc09ad354be14f18
Lord-Zomm <rafay.cheema@gmail.com>
2024-10-01 14:46:30 -0400

    s

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -27,8 +27,8 @@ const Auth = () => {
 
         const { username, password, phoneNumber } = form;
 
-        const URL = 'http://localhost:5000/auth';
-        // const URL = 'https://medical-pager.herokuapp.com/auth';
+        //const URL = 'http://localhost:5000/auth';
+        const URL = 'https://synergy-45ua.onrender.com';
 
         const { data: { token, userId, hashedPassword, fullName } } = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`, {
             username, password, fullName: form.fullName, phoneNumber,
