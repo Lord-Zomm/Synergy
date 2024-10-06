6a492b3e370f5269a9432e6775885e489d487ebc
Lord-Zomm <rafay.cheema@gmail.com>
2024-10-06 18:12:41 -0400

    login

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -26,12 +26,11 @@ const Auth = () => {
 
         const { username, password } = form;
 
-        // Dynamically select protocol (http or https)
-        const protocol = window.location.protocol === 'https:' ? 'http' : 'http';
-        const baseURL = `${protocol}://ec2-3-144-146-156.us-east-2.compute.amazonaws.com:5000/auth`;
+        //const URL = 'http://localhost:5000/auth';
+        //const URL = 'https://synergy-45ua.onrender.com/auth';
+        const URL = 'https://ec2-3-144-146-156.us-east-2.compute.amazonaws.com:5000/auth';
 
-
-        const { data: { token, userId, hashedPassword, fullName } } = await axios.post(`${baseURL}/${isSignup ? 'signup' : 'login'}`, {
+        const { data: { token, userId, hashedPassword, fullName } } = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`, {
             username, password, fullName: form.fullName,
         });
 
@@ -122,4 +121,4 @@ const Auth = () => {
     )
 }
 
-export default Auth;
+export default Auth
