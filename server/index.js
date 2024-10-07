42449994496110c9f7a41fcc968606d5a805686a
Lord-Zomm <rafay.cheema@gmail.com>
2024-10-06 21:01:04 -0400

    -cert

--- server/index.js
+++ server/index.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const https = require('https');
-const fs = require('fs');
 
 const authRoutes = require("./routes/auth.js");
 
@@ -10,10 +8,9 @@ const PORT = process.env.PORT || 5000;
 
 require('dotenv').config();
 
-// Use CORS and JSON parsing middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded());
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
@@ -24,24 +21,13 @@ app.post('/', (req, res) => {
     const { type } = req.body;
 
     if (type === 'message.new') {
+        // Since Twilio logic has been removed, you can either respond here or implement any other logic you need
         return res.status(200).send('Message handling logic removed.');
     }
 
     return res.status(200).send('Not a new message request');
 });
 
-// Set up authentication routes
 app.use('/auth', authRoutes);
 
-// Load SSL certificate and key
-const privateKey = fs.readFileSync('/home/ec2-user/Synergy/server.key', 'utf8');
-const certificate = fs.readFileSync('/home/ec2-user/Synergy/server.cert', 'utf8');
-const credentials = { key: privateKey, cert: certificate };
-
-// Create HTTPS server
-const httpsServer = https.createServer(credentials, app);
-
-// Start the HTTPS server
-httpsServer.listen(PORT, () => {
-    console.log(`HTTPS Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
