53496c6e05f1ff2aca6db4b498d4e65a63c28800
Lord-Zomm <rafay.cheema@gmail.com>
2024-10-06 22:05:36 -0400

    gh

--- server/index.js
+++ server/index.js
@@ -1,17 +1,14 @@
 const express = require('express');
 const cors = require('cors');
-const authRoutes = require('./routes/auth');
+
+const authRoutes = require("./routes/auth.js");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// CORS configuration
-app.use(cors({
-    origin: 'https://synergysphere.vercel.app', // Allow requests only from this origin
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Allowed HTTP methods
-    credentials: true,                          // Allow cookies or authentication headers
-}));
+require('dotenv').config();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 
@@ -22,3 +19,4 @@ app.get('/', (req, res) => {
 app.use('/auth', authRoutes);
 
 app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
+
