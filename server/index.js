c3aaf650c35c4c555c7df180593ba9c93e5095bc
Lord-Zomm <rafay.cheema@gmail.com>
2024-10-06 21:42:31 -0400

    j

--- server/index.js
+++ server/index.js
@@ -16,18 +16,7 @@ app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
-// Removed Twilio-related message handling
-app.post('/', (req, res) => {
-    const { type } = req.body;
-
-    if (type === 'message.new') {
-        // Since Twilio logic has been removed, you can either respond here or implement any other logic you need
-        return res.status(200).send('Message handling logic removed.');
-    }
-
-    return res.status(200).send('Not a new message request');
-});
-
 app.use('/auth', authRoutes);
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
+
