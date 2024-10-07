bf5047c9cbc411293630d85ba57b7ead2bbe9308
Lord-Zomm <rafay.cheema@gmail.com>
2024-10-06 22:30:56 -0400

    k

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -27,9 +27,9 @@ const Auth = () => {
         const { username, password } = form;
 
         //const URL = 'http://localhost:5000/auth';
-        //const URL = 'https://synergy-45ua.onrender.com/auth';
+        const URL = 'https://synergy-45ua.onrender.com/auth';
         //const URL = 'http://ec2-3-144-146-156.us-east-2.compute.amazonaws.com:5000/auth';
-        const URL = 'https://74a7-3-144-146-156.ngrok-free.app/auth';
+        //const URL = 'https://74a7-3-144-146-156.ngrok-free.app/auth';
 
         const { data: { token, userId, hashedPassword, fullName } } = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`, {
             username, password, fullName: form.fullName,
